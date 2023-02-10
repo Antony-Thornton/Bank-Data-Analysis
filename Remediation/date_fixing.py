8e8e09274da9087ea52b70912b7970d9fefaa15f
Antony-Thornton <79005552+Antony-Thornton@users.noreply.github.com>
2023-02-10 21:39:06 +0000

    Added date fixing function.

---
+++ Remediation/date_fixing.py
@@ -0,0 +1,17 @@
+# Import libraries
+import pandas as pd
+import numpy
+
+# Import scripts
+import Data.get_data
+
+
+def fix_date(df):
+    print("Hello world")
+
+
+def all_def():
+    fix_date(df)
+
+
+df = Data.get_data.create_data_frame()
--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 # Import scripts
 import Data.get_data
 import visuals.visuals
+import Remediation.date_fixing
 
 
 def main():
     print("Data Frame imported.", end='\n\n')
 
     df = Data.get_data.create_data_frame()
-    visuals.visuals.all_def()
+    # visuals.visuals.all_def()
+    Remediation.date_fixing.all_def()
 
     return df
 
